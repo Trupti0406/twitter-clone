d3e96f28ce805295cab718303168a21e78c10555
trupti0406 <truptivijayyadav27@gmail.com>
2023-06-13 19:58:22 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 const authRoutes = require("./routes/authRoutes.js");
 const tweetRoutes = require("./routes/tweetRoutes.js");
@@ -12,7 +12,12 @@ const cloudinary = require("./utilities/cloudinaryConfig.js"); // Require the cl
 
 dotenv.config();
 // to get rid of CORS (cross-origin resource sharing) error
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://twitter-server-2mbt.onrender.com",
+  })
+);
+
 app.use(express.json());
 
 // Routes
